205f4f18ce00f7c4f4aa7cabc71a8e29e2b6c81f
Ahsanur Rahman <ahsanniloy787@gmail.com>
2023-06-23 12:05:34 +0600

    Responsive design added .But Delete function is missing

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -1,5 +1,7 @@
+import { Component } from "react";
 import styles from "./ListContainer.module.css";
 const ListContainer = (props) => {
+  // wish holder Component
   return (
     <div className={styles.listContainer}>
       <div className={styles.allExercises}>{props.Wishes.wish}</div>
